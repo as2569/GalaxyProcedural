c23a6718df554bb0a1246b861481b910f1379798
as2569 <as2569@njit.edu>
2016-12-23 01:13:52 -0500

    planets will instantiate at random positions within given range

--- Spawn.cs
+++ Spawn.cs
@@ -4,65 +4,56 @@ using UnityEngine;
 
 public class Spawn : MonoBehaviour
 {
-    public int num_nodes = 10; //Number of nodes
+    public const int NUM_NODES = 10; //Number of nodes
     public float xmax = 10.0f; //Max distance from origin at x axis
     public float ymax = 10.0f; //Max distance from origin at y axis
     public float min_dis = 1.0f; //Min distance between nodes
-    ArrayList nodes = new ArrayList();
-    ArrayList edges = new ArrayList();
-    public GameObject node;
+    Vector3[] nodes = new Vector3[NUM_NODES];
+    //ArrayList edges = new ArrayList();
+    public GameObject planet;
 
-    //Propose a node at random positions and make sure it will not overlap other nodes
-    public bool random_node()
+    //Create a random node after checking it won't overlap any other nodes
+    public Vector3 random_node()
     {
         //Random values within max distance
         float xpos = Random.Range(0, xmax);
         float ypos = Random.Range(0, ymax);
 
         //Proposed node
-        Vector3 proposed_node = new Vector3(xpos, ypos, 0);
+        Vector3 next_node;
+        next_node = new Vector3(xpos, ypos, 0);
 
         //Check each existing node for overlap
-        foreach(Vector3 node in nodes)
+        for(int i = 0; i < NUM_NODES; i++)
         {
-            
-            float dist = Vector3.Distance((node), (proposed_node)); 
-
-            if(dist < min_dis)
+            float dist = Vector3.Distance((nodes[i]), (next_node)); 
+            if(dist <= min_dis)
             {
-                return false;
-            }
-            else
-            {
-                nodes.Add(proposed_node);
-                return true;
+                i = 0;
+                xpos = Random.Range(0, xmax);
+                ypos = Random.Range(0, ymax);
+                next_node = new Vector3(xpos, ypos, 0);
+                Debug.Log("Overlap");
             }
         }
-        return false;
+        Debug.Log("Success. Returning valid node");
+        return next_node;
     }
 
 	// Use this for initialization
 	void Start ()
     {
         //Create first node at origin
-        //nodes[0] = new Vector3(0,0,0);
-        nodes.Add(new Vector3(0, 0, 0));
+        nodes[0] = new Vector3(0, 0, 0);
+        Instantiate(planet, nodes[0], Quaternion.identity);
+        Debug.Log("Initial node");
 
-        //Create an array of valid nodes 
-        for (int i = 1; i <= num_nodes; i++)
+        //Create a valid node 
+        for (int j = 0; j < 10; j++)
         {
-            if(!random_node())
-            {
-                i--;
-            }
-            
-            Debug.Log("Creating node number " + i);
+            random_node();
+            Instantiate(planet, random_node(), Quaternion.identity);
         }
-
-        for (int i = 0; i <= num_nodes; i++)
-        {
-            Instantiate(node, nodes[i]);
-        }
-	}
+    }
 
 }
